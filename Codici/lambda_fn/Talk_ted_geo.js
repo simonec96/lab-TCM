414a08a63619eb7234d8407fcede5c603b7526f3
StefanoScarpellini <s.scarpellini5@studenti.unibg.it>
2020-06-04 16:06:15 +0200

    Update Talk_ted_geo.js

--- Codici/lambda_fn/Talk_ted_geo.js
+++ Codici/lambda_fn/Talk_ted_geo.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 const talk_schema = new mongoose.Schema({
     title: String,
     url: String,
-    geo_area:{
-        continent: String,
-        nation:  String,
-        city: String
-    }
+    details: String,
+    main_speaker: String
     
 }, { collection: 'tedz_data' });
+
 
 module.exports = mongoose.model('talk', talk_schema);
