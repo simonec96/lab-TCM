e7b74135463979c1cca1ca9856a3cda104c273b9
StefanoScarpellini <s.scarpellini5@studenti.unibg.it>
2020-06-05 18:06:07 +0200

    Update Talk_vote_idx.js

--- Codici/lambda_fn/Talk_vote_idx.js
+++ Codici/lambda_fn/Talk_vote_idx.js
@@ -4,9 +4,11 @@ const talk_schema = new mongoose.Schema({
     _id: String,
     title: String,
     url:String,
-    vote_user: [{ vote: String, date: String, time: String, user_mail: String }]
+    main_speaker:String,
+    vote_user: [{vote:Number,mail_user:String}]
 }, { collection: 'tedz_data' });
 
 
+var Talk = mongoose.model('talk', talk_schema);
 
-module.exports = mongoose.model('talk', talk_schema);
+module.exports=Talk;
