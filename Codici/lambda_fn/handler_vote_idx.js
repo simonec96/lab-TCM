eddb5056753289d6237e00802d3a7b36b8b7aab0
StefanoScarpellini <s.scarpellini5@studenti.unibg.it>
2020-06-05 18:05:42 +0200

    Update handler_vote_idx.js

--- Codici/lambda_fn/handler_vote_idx.js
+++ Codici/lambda_fn/handler_vote_idx.js
@@ -12,6 +12,12 @@ module.exports.get_vote_by_idx = (event, context, callback) => {
         body = JSON.parse(event.body)
     }
     // set default
+    
+    /* parametri:
+    - voto unico, id e numero stelle 
+    - media voti
+    */
+    
     if(!body.id) {
         callback(null, {
                     statusCode: 500,
@@ -20,18 +26,53 @@ module.exports.get_vote_by_idx = (event, context, callback) => {
         })
     }
     
-
-    
     connect_to_db().then(() => {
         console.log('=> get_all idx of next talks');
-        talk.findOne({_id:body.id},{title:1, url:1, vote_user:1})
+        if(!body.voto_limite){
+        talk.findOne({_id:body.id},'title url main_speaker vote_user')
             .then(talks => {
-                    callback(null, {
-                        statusCode: 200,
-                        body: JSON.stringify(talks)
-                    })
+                    var bd=JSON.stringify(talks)
+                    var n=0
+                    var sum=0
+                    while(n<talks.vote_user.length){
+                        sum+=talks.vote_user[n].vote
+                    n+=1
                 }
+                const media=sum/n
+                bd=bd+JSON.stringify({"media voti":media,"voti totali":n})
+                callback(null, {
+                    statusCode: 200,
+                    body:bd
+                })
+            })
+            .catch(err =>
+                callback(null, {
+                    statusCode: err.statusCode || 400,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Could not fetch the talks.'
+                })
             )
+            }else{
+                talk.findOne({_id:body.id})
+                .then(talks => {
+                    var obj={"title":talks.title,"url":talks.url,"main_speaker":talks.main_speaker};
+                    var bd=JSON.stringify(obj)
+                    var n=0
+                    var sum=0
+                    while(n<talks.vote_user.length){
+                        sum+=talks.vote_user[n].vote
+                    if(talks.vote_user[n].vote>=new Number(body.voto_limite)){
+                        bd=bd+JSON.stringify(talks.vote_user[n])
+                    }
+                    n+=1
+                }
+                const media=sum/n
+                bd=bd+JSON.stringify({"media voti":media,"voti totali":n})
+                callback(null, {
+                    statusCode: 200,
+                    body:bd
+                })
+            })
             .catch(err =>
                 callback(null, {
                     statusCode: err.statusCode || 400,
@@ -39,5 +80,6 @@ module.exports.get_vote_by_idx = (event, context, callback) => {
                     body: 'Could not fetch the talks.'
                 })
             );
-    });
+        }
+    })
 };
